18b2b85406b76cca89f80239b6f86fcab014b7c1
resteve <resteve@zikzakmedia.com>
2014-04-03 13:43:13 +0200

    Rename Class Configuration Sale Payment Type

--- configuration.py
+++ configuration.py
@@ -10,8 +10,8 @@ __metaclass__ = PoolMeta
 
 class ConfigurationSalePaymentType(ModelSQL, ModelView):
     'Configuration - Sale Payment Type'
-    __name__ = 'sale.configuration-account.payment.type'
-    _table = 'sale_configuration_account_payment_type_rel'
+    __name__ = 'sale.configuration-sale.payment.type'
+    _table = 'sale_configuration_sale_payment_type_rel'
     sale_configuration = fields.Many2One('sale.configuration',
         'Sale Configuration', ondelete='CASCADE', select=True)
     payment_type = fields.Many2One('account.payment.type', 'Payment Type',
@@ -21,7 +21,7 @@ class ConfigurationSalePaymentType(ModelSQL, ModelView):
 class Configuration:
     __name__ = 'sale.configuration'
     cashondelivery_payments = fields.Many2Many(
-        'sale.configuration-account.payment.type', 'sale_configuration',
+        'sale.configuration-sale.payment.type', 'sale_configuration',
         'payment_type', 'Cash on Delivery Payment Types',
         domain=[('kind', '=', 'receivable')],
         )
