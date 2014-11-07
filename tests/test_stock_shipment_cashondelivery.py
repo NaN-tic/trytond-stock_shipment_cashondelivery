b07a16485b166087899c116d8f49169001bfecb6
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:47:56 +0100

    Cleanup tests

--- tests/test_stock_shipment_cashondelivery.py
+++ tests/test_stock_shipment_cashondelivery.py
@@ -7,31 +7,21 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class StockShipmentCashondeliveryTestCase(unittest.TestCase):
-    '''
-    Test Stock Shipment Cashondelivery module.
-    '''
+    'Test Stock Shipment Cashondelivery module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module(
             'stock_shipment_cashondelivery')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('stock_shipment_cashondelivery')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
