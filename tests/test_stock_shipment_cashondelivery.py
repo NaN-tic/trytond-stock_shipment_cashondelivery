80dea085150fcda93b04573020175b1e01eb7e87
resteve <resteve@zikzakmedia.com>
2015-12-01 11:26:26 +0100

    Tests

--- tests/test_stock_shipment_cashondelivery.py
+++ tests/test_stock_shipment_cashondelivery.py
@@ -1,28 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the stock_shipment_cashondelivery module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends, test_view
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockShipmentCashondeliveryTestCase(unittest.TestCase):
+class StockShipmentCashondeliveryTestCase(ModuleTestCase):
     'Test Stock Shipment Cashondelivery module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'stock_shipment_cashondelivery')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_shipment_cashondelivery')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_shipment_cashondelivery'
 
 
 def suite():
@@ -30,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockShipmentCashondeliveryTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
